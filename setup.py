80c1b4ea265f63e7b4b43f54274feefd99802ad7
Oliver Zott <zott_oliver@web.de>
2024-05-01 13:53:10 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='1.0.0',
+    description='A short description of your project',
+    author='Your Name',
+    author_email='your_email@example.com',
+    url='https://github.com/your_username/your_project',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your project requires
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
+
+
+
