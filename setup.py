baccfce0bbb3caab3994d55b3c9d1f4489600de6
Oliver Zott <zott_oliver@web.de>
2024-05-01 14:13:07 +0200

    Basic project additions

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='your_project_name',
-    version='1.0.0',
-    description='A short description of your project',
-    author='Your Name',
-    author_email='your_email@example.com',
-    url='https://github.com/your_username/your_project',
+    name="your_project_name",
+    version="1.0.0",
+    description="A short description of your project",
+    author="Your Name",
+    author_email="your_email@example.com",
+    url="https://github.com/your_username/your_project",
     packages=find_packages(),
     install_requires=[
         # Add any dependencies your project requires
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
-
-
-
